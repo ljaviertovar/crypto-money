b45d7b14892d15d4a52d3aca2c44d912f757d76d
ljaviertovar <luisjavier_tovar@outlook.com>
2021-02-26 22:53:44 -0600

    [ADD] Initalize project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from '@emotion/styled';
+
+import imagen from './cryptomonedas.png';
+
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  @media (min-width:992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Image = styled.img`
+  max-width: 100%;
+  margin-top: 5rem;
+`;
+const Heading = styled.h1`
+  font-family: 'Bebas Neue', cursive;
+  color: #FFF;
+  text-align:left;
+  font-weight: 700;
+  font-size: 50px;
+  margin-bottom: 50px;
+  margin-top: 80px;
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display:block;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Container>
+    <div>
+      <Image 
+        src={imagen}
+        alt="image crypto"
+      />
     </div>
+    <div>
+        <Heading>Crypto-Money</Heading>
+
+       
+
+        
+    </div>
+</Container>
   );
 }
 
